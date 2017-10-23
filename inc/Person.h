4fc5b761aed2da555b594af39e4a9f5a0000f841
Rudrabhoj Bhati <harshvardhan.rawlot@gmail.com>
2017-10-23 05:00:24 +0000

    Added basic method defs

--- inc/Person.h
+++ inc/Person.h
@@ -1,1 +1,10 @@
+struct Person {
+  char *name;
+  int age;
+};
+
+void Person_new(char *name, int age);
+void Person_setAge(Person *per, int newAge);
+void Person_setName(Person *per, char *newName);
+
 void showWelcomeMessage();
