beb219a780f10fca992ce739a9134379f53f22df
Rudrabhoj Bhati <harshvardhan.rawlot@gmail.com>
2017-10-23 05:00:50 +0000

    Simplifying the message

--- src/Person.c
+++ src/Person.c
@@ -8,9 +8,5 @@ int main(int argc, char **argv) {
 }
 
 void showWelcomeMessage() {
-  printf("Hello and Welcome to my C Moduler Programming Test.\n");
-  printf("I am trying to go back to C.\n");
-  printf("I am trying to learn modular programming in C.\n");
-  printf("This project would be updated as I go.\n");
-  printf("\n\n---- Rudrabhoj Bhati\n");
+  printf("Success!\n");
 }
